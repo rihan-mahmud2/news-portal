161c06638f7ef323fe88f713c67b8dadd219f2cc
raju <rihanmahmud1415@gmail.com>
2022-09-02 23:31:15 +0600

    sucessfully card section added

--- script.js
+++ script.js
@@ -31,10 +31,31 @@ const displayNews = (news) => {
 
 const displayNewsCard = (eachNews) => {
   const { data } = eachNews;
-    data.forEach(singleData => {
+  data.forEach((singleData) => {
     console.log(singleData);
-    
-    })
+    const cardContainer = document.getElementById("card-container");
+    const div = document.createElement("div");
+    div.innerHTML = `
+      
+      <div class="card mb-3" style="max-width: 1040px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="${singleData.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+      
+      
+      `;
+    cardContainer.appendChild(div);
+  });
 };
 
 displayList();
