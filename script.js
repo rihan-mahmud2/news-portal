3aee345e05ca6d00a08f2486475217451b39a15a
raju <rihanmahmud1415@gmail.com>
2022-09-02 23:58:45 +0600

    modal has been added

--- script.js
+++ script.js
@@ -31,22 +31,36 @@ const displayNews = (news) => {
 
 const displayNewsCard = (eachNews) => {
   const { data } = eachNews;
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.innerHTML = "";
   data.forEach((singleData) => {
     console.log(singleData);
-    const cardContainer = document.getElementById("card-container");
     const div = document.createElement("div");
+
     div.innerHTML = `
       
       <div class="card mb-3" style="max-width: 1040px;">
   <div class="row g-0">
     <div class="col-md-4">
-      <img src="${singleData.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+      <img src="${
+        singleData.thumbnail_url
+      }" class="img-fluid rounded-start" alt="...">
     </div>
     <div class="col-md-8">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        <h5 class="card-title">${singleData.title}</h5>
+        <p class="card-text">${singleData.details.slice(0, 200)}...</p>
+        <div class="d-flex flex-row  justify-content-evenly align-items-center">
+        <img src="${
+          singleData.author.img
+        }" alt="..." class="img-fluid" style="width:100px">
+        <p>Viewrs: ${
+          singleData.total_view ? singleData.total_view : "No data Found"
+        }</p>
+
+        <button class="btn btn-primary">Click</button>
+        
+        </div>
       </div>
     </div>
   </div>
